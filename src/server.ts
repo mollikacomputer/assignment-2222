32423bbdf81879048c250e4de007a5ee844e6b9d
mollikacomputer <mollikacomputer3@gmail.com>
2026-05-22 20:33:43 +0600

    get single user api created

--- src/server.ts
+++ src/server.ts
@@ -39,6 +39,8 @@ const initDB = async () => {
   }
 };
 initDB();
+
+
 // home page get api
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
@@ -46,8 +48,42 @@ app.get("/", (req: Request, res: Response) => {
     author: "Ranjit Kumar Mandal",
   });
 });
-// get Api get all user
 
+// get single user
+
+app.get("/api/users/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(
+      `
+      SELECT * FROM users WHERE id=$1  
+        `,
+      [id],
+    );
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User Not found!",
+        data: {},
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User retrived successfully!",
+      data: result.rows[0],
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error,
+    });
+  }
+});
+
+// getApi get all user
 app.get("/api/users", async (req: Request, res: Response) => {
   try {
     const result = await pool.query(`
